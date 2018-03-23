a3dc9c28c1c84c10c5098764d4c592e361f1e5f1
Junichi Sato <sat0b.dev@gmail.com>
2018-03-23 20:05:04 +0900

    Rename test cases

--- test/Tests.cpp
+++ test/Tests.cpp
@@ -2,25 +2,25 @@
 #include "../src/XMLParser.h"
 #include "../src/XMLTree.h"
 
-TEST(unitTests, test1) {
+TEST(unitTests, readSingleElementTag) {
     XMLParser xmlParser("<foo>bar</foo>");
     XMLTree *xmlTree = xmlParser.parse();
     EXPECT_EQ("foo", xmlTree->elements["foo"]->tag);
 }
 
-TEST(unitTests, test2) {
+TEST(unitTests, readSingleElement) {
     XMLParser xmlParser("<foo>bar</foo>");
     XMLTree *xmlTree = xmlParser.parse();
     EXPECT_EQ("bar", xmlTree->elements["foo"]->text);
 }
 
-TEST(unitTests, test3) {
+TEST(unitTests, readNestedElementText) {
     XMLParser xmlParser("<foo><bar>baz</bar></foo>");
     XMLTree *xmlTree = xmlParser.parse();
     EXPECT_EQ("baz", xmlTree->elements["foo"]->elements["bar"]->text);
 }
 
-TEST(unitTests, test4) {
+TEST(unitTests, readNestedMultipleElement) {
     XMLParser xmlParser("<foo><bar1>baz1</bar1><bar2>baz2</bar2></foo>");
     XMLTree *xmlTree = xmlParser.parse();
     EXPECT_EQ("baz1", xmlTree->elements["foo"]->elements["bar1"]->text);
