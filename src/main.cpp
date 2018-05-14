a9bf0fe735dd797d5af89d5e0afcc69913335880
Junichi Sato <sat0b.dev@gmail.com>
2018-05-14 21:56:23 +0900

    add App class

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,63 @@
-#include <iostream>
-#include "XmlParser.h"
-#include "XmlTree.h"
-#include "Tokenizer.h"
-#include "WikiLoader.h"
 #include "Document.h"
 #include "Indexer.h"
 #include "Searcher.h"
+#include "Tokenizer.h"
+#include "WikiLoader.h"
+#include "XmlParser.h"
+#include "XmlTree.h"
+#include <iostream>
 
-void run_command_line(std::string file_name) {
-    XmlParser xml_parser;
-    xml_parser.open(file_name);
-    WikiLoader wiki_loader(xml_parser.parse());
-    std::vector<Document> documents = wiki_loader.load();
-    Indexer indexer(documents);
-    indexer.output_storage();
-    Searcher searcher;
+class App {
+  public:
+    void save_index(std::string file_name) {
+        XmlParser xml_parser;
+        xml_parser.open(file_name);
+        WikiLoader wiki_loader(xml_parser.parse());
+        std::vector<Document> documents = wiki_loader.load();
+        Indexer indexer(documents);
+        indexer.output_storage();
+    }
 
-    std::string query;
-    for (;;) {
-        std::cout << "Query: ";
-        std::cin >> query;
-        if (query == "exit")
-            break;
+    void search() {
+        std::string query;
+        for (;;) {
+            std::cout << "Query: ";
+            std::cin >> query;
+            if (query == "exit")
+                break;
 
-        std::vector<int> id_list = searcher.search(query);
-        if (id_list.size() == 0) {
-            std::cout << "Zero match" << std::endl;
-            continue;
-        }
+            std::vector<int> id_list = searcher_.search(query);
+            if (id_list.empty()) {
+                std::cout << "Zero match" << std::endl;
+                continue;
+            }
 
-        for (int id : id_list) {
-            std::cout << "document_id: " << id << "\n";
+            for (int id : id_list) {
+                std::cout << "document_id: " << id << "\n";
+            }
         }
     }
-}
+
+    void usage() {
+        std::cout << "Usage: kajiki:\n";
+        std::cout << "  -f string\n";
+        std::cout << "      file_name (xml)\n";
+        std::cout << "  -s\n";
+        std::cout << "      server mode\n";
+    }
+
+  private:
+    Searcher searcher_;
+};
 
 int main(int argc, char **argv) {
-    if (argc == 2) {
-        run_command_line(argv[1]);
+    App app;
+    if (argc == 1) {
+        app.search();
+    } else if (argc == 2) {
+        app.save_index(argv[1]);
+    } else {
+        app.usage();
+        exit(1);
     }
 }
