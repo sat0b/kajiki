bc06297fda301e9ef5c85593fe7c36d2f5c376d0
Junichi Sato <sat0b.dev@gmail.com>
2018-05-19 08:30:48 +0900

    Add test for searcher

--- test/SearcherTest.cpp
+++ test/SearcherTest.cpp
@@ -16,3 +16,18 @@ TEST(seacher_test, searcher_test) {
     ASSERT_THAT(searcher.search("ab"), ElementsAre(1, 2));
     ASSERT_THAT(searcher.search("cd"), ElementsAre(2, 3));
 }
+
+TEST(seacher_test, searcher_initialized_by_storage_index) {
+    // output index
+    std::vector<Document> documents;
+    documents.emplace_back(1, "ab", "");
+    documents.emplace_back(2, "abcd", "");
+    documents.emplace_back(3, "cd", "");
+    Indexer indexer(documents);
+    indexer.output_storage();
+
+    // search
+    Searcher searcher;
+    ASSERT_THAT(searcher.search("ab"), ElementsAre(1, 2));
+    ASSERT_THAT(searcher.search("cd"), ElementsAre(2, 3));
+}
