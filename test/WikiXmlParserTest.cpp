d126d5eccd8de8a01f9cb2fe7953d3b9ae61442d
Junichi Sato <sat0b.dev@gmail.com>
2018-05-19 08:33:32 +0900

    Add test for searcher

--- test/WikiXmlParserTest.cpp
+++ test/WikiXmlParserTest.cpp
@@ -1,7 +1,7 @@
 #include "../src/WikiXmlParser.h"
 #include <gtest/gtest.h>
 
-TEST(wiki_xml_parser_test, test1) {
+TEST(wiki_xml_parser_test, xml_parse_test) {
     WikiXmlParser wiki_xml_parser("xml/test.xml");
     wiki_xml_parser.parse();
 }
