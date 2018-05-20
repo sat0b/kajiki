5e4b2f076df462239cd209a930d1d5a010fd6297
Junichi Sato <sat0b.dev@gmail.com>
2018-05-20 23:47:20 +0900

    Modify test

--- test/WikiXmlParserTest.cpp
+++ test/WikiXmlParserTest.cpp
@@ -4,6 +4,7 @@
 TEST(wiki_xml_parser_test, xml_parse_test) {
     WikiXmlParser wiki_xml_parser("xml/test.xml", 10);
     std::vector<Document> documents = wiki_xml_parser.parse_next();
+    ASSERT_EQ(documents.size(), 2);
     ASSERT_EQ(documents[0].title, "Title1");
     ASSERT_EQ(documents[1].title, "Title2");
 }
